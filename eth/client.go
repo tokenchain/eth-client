f73723149ac252239080ad2464aedcedadea1382
jhes <ooxfordck@gmail.com>
2018-11-16 14:22:53 +0800

    remove tx object

--- eth/client.go
+++ eth/client.go
@@ -44,31 +44,31 @@ type ClientTokenEth struct {
 }
 
 type RpcEthTransaction struct {
-	Data Txdata
+	Txdata
 	txExtraInfo
 }
 
 type Txdata struct {
-	AccountNonce uint64          `json:"nonce"    gencodec:"required"`
-	Price        *big.Int        `json:"gasPrice" gencodec:"required"`
-	GasLimit     uint64          `json:"gas"      gencodec:"required"`
-	Recipient    *common.Address `json:"to"       rlp:"nil"` // nil means contract creation
-	Amount       *big.Int        `json:"value"    gencodec:"required"`
-	Payload      []byte          `json:"input"    gencodec:"required"`
+	AccountNonce string         `json:"nonce"    gencodec:"required"`
+	Price        string         `json:"gasPrice"   gencodec:"required"`
+	GasLimit     string         `json:"gas"      gencodec:"required"`
+	Recipient    common.Address `json:"to"       rlp:"nil"` // nil means contract creation
+	Amount       string         `json:"value"    gencodec:"required"`
+	Payload      []byte         `json:"input"    gencodec:"required"`
 
 	// Signature values
-	V *big.Int `json:"v" gencodec:"required"`
-	R *big.Int `json:"r" gencodec:"required"`
-	S *big.Int `json:"s" gencodec:"required"`
+	V string `json:"v" gencodec:"required"`
+	R string `json:"r" gencodec:"required"`
+	S string `json:"s" gencodec:"required"`
 
 	// This is only used when marshaling to JSON.
-	Hash *common.Hash `json:"hash" rlp:"-"`
+	Hash common.Hash `json:"hash" rlp:"-"`
 }
 
 type txExtraInfo struct {
-	BlockNumber *string         `json:"blockNumber,omitempty"`
-	BlockHash   *common.Hash    `json:"blockHash,omitempty"`
-	From        *common.Address `json:"from,omitempty"`
+	BlockNumber string         `json:"blockNumber,omitempty"`
+	BlockHash   common.Hash    `json:"blockHash,omitempty"`
+	From        common.Address `json:"from,omitempty"`
 }
 
 // Dial connects a client to the given URL.
