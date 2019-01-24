44c146324a3ae027be28bdd7897c63497c497368
jhes <ooxfordck@gmail.com>
2019-01-24 21:07:39 +0800

    add not

--- client/api.go
+++ client/api.go
@@ -2,12 +2,53 @@ package client
 
 import (
 	"context"
+	"math/big"
+	"github.com/ethereum/go-ethereum/p2p"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum"
 )
 
 type Client interface {
-	Close()
-	GetInfo(ctx context.Context) (string, error)
-	GenerateKey(ctx context.Context) (address, private string, err error)
-	SendAmount(ctx context.Context, fromPriv, toPub, amount string) error
-	GetBalance(ctx context.Context, account string) (string, error)
+	// eth
+	BlockNumber(ctx context.Context) (*big.Int, error)
+	SendRawTransaction(ctx context.Context, tx *types.Transaction) error
+
+	// admin
+	AddPeer(ctx context.Context, nodeURL string) error
+	AdminPeers(ctx context.Context) ([]*p2p.PeerInfo, error)
+	NodeInfo(ctx context.Context) (*p2p.PeerInfo, error)
+
+	// miner
+	StartMining(ctx context.Context) error
+	StopMining(ctx context.Context) error
+
+	// eth client
+	BlockByHash(ctx context.Context, hash common.Hash) (*types.Block, error)
+	BlockByNumber(ctx context.Context, number *big.Int) (*types.Block, error)
+	HeaderByHash(ctx context.Context, hash common.Hash) (*types.Header, error)
+	HeaderByNumber(ctx context.Context, number *big.Int) (*types.Header, error)
+	TransactionByHash(ctx context.Context, hash common.Hash) (*types.Transaction, bool, error)
+	TransactionCount(ctx context.Context, blockHash common.Hash) (uint, error)
+	TransactionInBlock(ctx context.Context, blockHash common.Hash, index uint) (*types.Transaction, error)
+	TransactionReceipt(ctx context.Context, txHash common.Hash) (*types.Receipt, error)
+	SyncProgress(ctx context.Context) (*ethereum.SyncProgress, error)
+	SubscribeNewHead(ctx context.Context, ch chan<- *types.Header) (ethereum.Subscription, error)
+	NetworkID(ctx context.Context) (*big.Int, error)
+	BalanceAt(ctx context.Context, account common.Address, blockNumber *big.Int) (*big.Int, error)
+	StorageAt(ctx context.Context, account common.Address, key common.Hash, blockNumber *big.Int) ([]byte, error)
+	CodeAt(ctx context.Context, account common.Address, blockNumber *big.Int) ([]byte, error)
+	NonceAt(ctx context.Context, account common.Address, blockNumber *big.Int) (uint64, error)
+	FilterLogs(ctx context.Context, q ethereum.FilterQuery) ([]types.Log, error)
+	SubscribeFilterLogs(ctx context.Context, q ethereum.FilterQuery, ch chan<- types.Log) (ethereum.Subscription, error)
+	PendingBalanceAt(ctx context.Context, account common.Address) (*big.Int, error)
+	PendingStorageAt(ctx context.Context, account common.Address, key common.Hash) ([]byte, error)
+	PendingCodeAt(ctx context.Context, account common.Address) ([]byte, error)
+	PendingNonceAt(ctx context.Context, account common.Address) (uint64, error)
+	PendingTransactionCount(ctx context.Context) (uint, error)
+	CallContract(ctx context.Context, msg ethereum.CallMsg, blockNumber *big.Int) ([]byte, error)
+	PendingCallContract(ctx context.Context, msg ethereum.CallMsg) ([]byte, error)
+	SuggestGasPrice(ctx context.Context) (*big.Int, error)
+	EstimateGas(ctx context.Context, msg ethereum.CallMsg) (uint64, error)
+	SendTransaction(ctx context.Context, tx *types.Transaction) error
 }
